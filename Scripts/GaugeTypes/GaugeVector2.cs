38130d90f008fbd458fe1eb427310890b447db10
David Barlia <barliesque@gmail.com>
2024-10-04 11:41:06 -0700

    Added GaugeVector2

---
+++ Scripts/GaugeTypes/GaugeVector2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Barliesque.EventObjects
+{
+	[CreateAssetMenu(fileName = "Gauge(Vector2)", menuName = "Barliesque/Event Objects/Gauges/Vector2", order = 1)]
+	public class GaugeVector2 : Gauge<Vector2>, Gauge<Vector2>.ISerializable
+	{
+		public string Serialize(Vector2 value)
+		{
+			return $"{value.x},{value.y}";
+		}
+
+		public Vector2 Deserialize(string serial)
+		{
+			var part = serial.Split(',');
+			return new Vector2(float.Parse(part[0]), float.Parse(part[1]));
+		}
+	}
+
+}
